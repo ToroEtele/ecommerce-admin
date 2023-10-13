ed343ac0658957c400afdf4d350f085803c5febe
ToroEtele <toroetele@gmail.com>
2023-10-13 14:40:08 +0300

    Implement billboards data table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,11 +3,18 @@
 import { useParams, useRouter } from "next/navigation";
 import { Plus } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/ui/api-list";
 import { Heading } from "@/components/ui/heading";
-import { Separator } from "@radix-ui/react-separator";
+import { Button } from "@/components/ui/button";
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -15,7 +22,7 @@ const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -24,8 +31,12 @@ const BillboardClient = () => {
           <Plus className="mr-2 h-4 w-4" />
           Add new
         </Button>
-        <Separator />
       </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
