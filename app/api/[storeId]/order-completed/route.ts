be86bbedd22910406b3161ebef394cf8d873fcfb
ToroEtele <toroetele@gmail.com>
2023-10-23 17:51:20 +0300

    API order-completed start implementation

---
+++ app/api/[storeId]/order-completed/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const {
+    name,
+    email,
+    phone,
+    address,
+    city,
+    state,
+    zip,
+    isCompany,
+    cmp_name,
+    cmp_city,
+    cmp_cui,
+    cmp_address,
+    cmp_state,
+    cmp_bank,
+    cmp_cont,
+  } = body;
+
+  if (!name) return new NextResponse("Name is required", { status: 400 });
+  if (!email) return new NextResponse("Email is required", { status: 400 });
+  if (!phone) return new NextResponse("Phone is required", { status: 400 });
+  if (!address) return new NextResponse("Address is required", { status: 400 });
+  if (!city) return new NextResponse("City is required", { status: 400 });
+  if (!state) return new NextResponse("State is required", { status: 400 });
+  if (!zip) return new NextResponse("Zip is required", { status: 400 });
+  if (isCompany) {
+    if (!cmp_name)
+      return new NextResponse("Company name is required", { status: 400 });
+    if (!cmp_city)
+      return new NextResponse("Company city is required", { status: 400 });
+    if (!cmp_cui)
+      return new NextResponse("Company cui is required", { status: 400 });
+    if (!cmp_address)
+      return new NextResponse("Company address is required", { status: 400 });
+    if (!cmp_state)
+      return new NextResponse("Company state is required", { status: 400 });
+    if (!cmp_bank)
+      return new NextResponse("Company bank is required", { status: 400 });
+    if (!cmp_cont)
+      return new NextResponse("Company contact is required", { status: 400 });
+  }
+}
